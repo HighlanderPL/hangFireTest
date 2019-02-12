1fa8d4c1a7a68dc2eadc5afcdc756e32dffca861
Pawlikowski <Adam.Pawlikowski@coxautoinc.com>
2019-02-12 18:00:45 -0500

    first commit

---
+++ hangFireTest/Controllers/AppController.cs
@@ -0,0 +1,25 @@
+﻿using hangFireTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hangFireTest.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly HangFireTask _task;
+        
+        public AppController(HangFireTask task)
+        {
+            _task = task;
+        }
+        public IActionResult Index()
+        {
+            ViewBag.message = _task.message;
+            ViewBag.now = _task.now;   
+            return View();
+        }
+    }
+}
---
+++ hangFireTest/Models/HangFireTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace hangFireTest.Models
+{
+    public class HangFireTask
+    {
+        public HangFireTask()
+        {
+            UpdateTime();
+        }
+        
+        public DateTime now;
+        public string message = "The UpdateTime method last ran: ";
+
+        public void UpdateTime ()
+        {
+            now = DateTime.Now;
+        }
+    }
+}
